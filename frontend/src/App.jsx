32101e6d0c3faa0cd8584867f7493097e2f320ac
Abdulrahman <abdulrahman.almhawsh@dci-student.or>
2025-04-07 13:44:15 +0200

    adding Carousel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
+import Carousel from './components/Carousel'
 
 function App() {
 
   return (
     <>
       <Navbar />
-      
+      <Carousel/>
       <Footer />
     </>
   )
