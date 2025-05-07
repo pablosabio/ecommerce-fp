2bb35f24e8759571d5277a2df966503f58a3576e
Royce <royce.burghardt@dci-student.org>
2025-05-07 11:17:01 +0200

    users-new

--- backend/server.js
+++ backend/server.js
@@ -44,58 +44,10 @@ app.use('/api/stripe', stripeRoutes);
 
 //I want to add also this
 //for userRoutes
-//app.use('/api/users', userRoutes);
+app.use('/api/users', userRoutes);
 
 
-import { Schema, model } from "mongoose";
-import { addressSchema } from "./addressSchema.js";
 
-/*const userSchema = new Schema(
-  {
-    first_name: {
-      type: String,
-      required: [true, "Please provide your first name!"],
-    },
-    last_name: {
-      type: String,
-      required: [true, "Please provide your last name!"],
-    },
-    email: {
-      type: String,
-      required: [true, "Please provide an email address!"],
-      unique: [true, "Email must be unique"],
-      lowercase: true,
-      trim: true,
-    },
-    role: {
-      type: String,
-      enum: ["user"],
-      default: "user",
-    },
-    password: {
-      type: String,
-      required: [true, "Please provide a password!"],
-      minlength: [6, "Password must be at least 6 characters long"],
-    },
-    profile_avatar: {
-      type: String,
-      default() {
-        return `https://robohash.org/${this.last_name}`;
-      },
-    },
-    address: {
-      type: addressSchema,
-      required: false,
-    },
-    orders: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Order",
-      },
-    ],
-  },
-  { timestamps: true }
-);*/
 
 // Create and export the User model
 /*const UsersModel = model("User", userSchema);
