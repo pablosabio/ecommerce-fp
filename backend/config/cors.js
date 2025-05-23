0b3c4935eafe30079f9523b4a99ed7b43c60c416
pablosabio <pablo.sabio@dci-student.org>
2025-05-23 10:51:46 +0200

    Update CORS to allow new Netlify domain

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -1,6 +1,6 @@
 const corsOptions = {
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://682f1afc4fab403ab204cf9c--quickcart-fp.netlify.app', 'http://localhost:5173'] 
+    ? true // Temporarily allow all origins
     : 'http://localhost:5173',
   credentials: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
